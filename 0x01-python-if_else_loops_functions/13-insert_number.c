e1cfd193e52bdae97ee28b340d8f062bd516914e
njakwa <greychakhalira@gmail.com>
2022-11-30 07:23:19 +0000

    commit

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,45 +1,39 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 /**
- * insert_node - inserts a node
- * @head: head
- * @number: int to add
- * Return: new node
+ * insert_node - inserts a number in an ordered linked list
+ * @head: double pointer to the linked list
+ * @number: number to insert in the new node
+ *
+ * Return: address of the new node, or NULL
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *temp = NULL, *node = NULL;
+	listint_t *new, *tmp, *prev;
 
-    if (head == NULL)
-        return (NULL);
-
-    node = malloc(sizeof(listint_t *));
-    if (node == NULL)
-        return (NULL);
-    node->next = NULL;
-    node->n = number;
-
-    temp = *head;
-    while (temp)
-    {
-        if (temp->n >= number)
-        {
-            node->next = temp;
-            *head = node;
-            return (node);
-        }
-        else if (temp->n <= number && temp->next->n >= number)
-        {
-            if (temp->next != NULL)
-            {
-                node->next = temp->next;
-                temp->next = node;
-                return (node);
-            }
-        }
-        temp = temp->next;
-    }
-
-    return (NULL);
+	tmp = *head;
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = number;
+	if (!*head || (*head)->n > number)
+	{
+		new->next = *head;
+		*head = new;
+		return (*head);
+	}
+	while (tmp)
+	{
+		if (tmp->n > number)
+		{
+			break;
+		}
+		prev = tmp;
+		tmp = tmp->next;
+	}
+	new->next = tmp;
+	prev->next = new;
+	return (*head);
 }
